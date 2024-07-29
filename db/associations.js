9a503cacbfeea323360450ac45b3adce0e39769f
hexzoner <hexzon@gmail.com>
2024-07-29 18:38:38 +0200

    tables associations

--- db/associations.js
+++ db/associations.js
@@ -1,6 +1,20 @@
+import sequelize from "./index.js";
+
 import User from "../models/user.js";
 import Order from "../models/order.js";
 import Product from "../models/product.js";
 import Category from "../models/category.js";
+import OrderProduct from "../models/orderProducts.js";
+
+User.hasMany(Order, { foreignKey: "userId" });
+Order.belongsTo(User, { foreignKey: "userId" });
+
+Category.hasMany(Product, { foreignKey: "categoryId" });
+Product.belongsTo(Category, { foreignKey: "categoryId" });
+
+Order.belongsToMany(Product, { through: OrderProduct });
+Product.belongsToMany(Order, { through: OrderProduct });
+
+sequelize.sync({ alter: true });
 
 export { User, Order, Product, Category };
