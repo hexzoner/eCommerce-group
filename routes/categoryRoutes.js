b708cbdbe8ce99550ac827d1322fcc9be9bead9e
hexzoner <hexzon@gmail.com>
2024-08-01 10:27:03 +0200

    polishing orders

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { getCategories, createCategory, getCategoryById, updateCategory, deleteCategory } from "../controllers/categories.js";
 import { asyncHandler } from "../middleware/errorHandler.js";
-import validate from "../middleware/validate.js";
-import categorySchema from "../schemas/categories.js";
 
 const categoryRouter = Router();
 
-categoryRouter.route("/").get(asyncHandler(getCategories)).post(validate(categorySchema), asyncHandler(createCategory));
-categoryRouter.route("/:id").get(asyncHandler(getCategoryById)).put(validate(categorySchema), asyncHandler(updateCategory)).delete(asyncHandler(deleteCategory));
+categoryRouter.route("/").get(asyncHandler(getCategories)).post(asyncHandler(createCategory));
+categoryRouter.route("/:id").get(asyncHandler(getCategoryById)).put(asyncHandler(updateCategory)).delete(asyncHandler(deleteCategory));
 
 export default categoryRouter;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { getProducts, createProduct, getProductById, updateProduct, deleteProduct } from "../controllers/products.js";
 import { asyncHandler } from "../middleware/errorHandler.js";
-import productSchema from "../schemas/products.js";
-import validate from "../middleware/validate.js";
 
 const productRouter = Router();
 
-productRouter.route("/").get(asyncHandler(getProducts)).post(validate(productSchema.POST), asyncHandler(createProduct));
-productRouter.route("/:id").get(asyncHandler(getProductById)).put(validate(productSchema.PUT), asyncHandler(updateProduct)).delete(asyncHandler(deleteProduct));
+productRouter.route("/").get(asyncHandler(getProducts)).post(asyncHandler(createProduct));
+productRouter.route("/:id").get(asyncHandler(getProductById)).put(asyncHandler(updateProduct)).delete(asyncHandler(deleteProduct));
 
 export default productRouter;
